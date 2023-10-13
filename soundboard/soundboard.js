caebd8c4bfcbc3b21db7bdbd4342e47b11c68493
ScottScripts <100329425+sc0ttferren@users.noreply.github.com>
2023-10-13 03:42:19 -0500

    key controls

--- soundboard/soundboard.js
+++ soundboard/soundboard.js
@@ -1,9 +1,11 @@
 // soundboard.js
 const soundButtons = document.querySelectorAll('.sound-button');
+let activeSounds = [];
 
 soundButtons.forEach(button => {
     button.addEventListener('click', () => {
         const sound = new Audio(button.dataset.sound);
+        activeSounds.push(sound);
         sound.play();
         button.classList.add('active');
         setTimeout(() => {
@@ -11,3 +13,17 @@ soundButtons.forEach(button => {
         }, sound.duration * 1000);
     });
 });
+
+document.addEventListener('keydown', event => {
+    // Check if Ctrl and S keys are pressed
+    if (event.ctrlKey && event.key === 's') {
+        stopAllSounds();
+    }
+});
+
+function stopAllSounds() {
+    activeSounds.forEach(sound => {
+        sound.pause();
+    });
+    activeSounds = [];
+}
